b300b235bdf96646514a1a0206b50cdbaae008d7
Hack Reactor Students <leebyp@gmail.com>
2014-05-07 16:38:56 -0700

    Serves index.html file at root

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,38 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var pathName = url.parse(req.url).pathname;
+  if (pathName === '/') {
+    if (req.method === 'GET') {
+      fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
+        //console.log('GETTINg Here', err, JSON.stringify(data.toString()));
+        if (err) {
+          throw err;
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(data);
+        }
+      });
+    }
+  } else if (pathName === '/styles.css') {
+    if (req.method === 'GET') {
+      fs.readFile(archive.paths.siteAssets + pathName, function(err, data) {
+        // console.log('GETTINg Here', err, JSON.stringify(data.toString()));
+        if (err) {
+          throw err;
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/css'});
+          res.end(data);
+        }
+      });
+    }
+  }
+
+
+
+//  res.end(archive.paths.list);
 };
