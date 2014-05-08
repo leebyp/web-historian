12b6666a7becb8dbee396926114d8e266dc9bbbf
Hack Reactor Students <leebyp@gmail.com>
2014-05-07 20:24:50 -0700

    Refactors request handler code into http helpers

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,9 +10,34 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
-exports.serveAssets = function(res, asset) {
+exports.serveAssets = function(res, asset, pathName, contentType, status) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  fs.readFile(asset + pathName, function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not Found');
+    } else {
+      res.writeHead(status || 200, {'Content-Type': contentType});
+      res.end(data);
+    }
+  });
 };
 
 // As you progress, keep thinking about what helper functions you can put here!
+
+exports.redirectToArchive = function(res, url){
+  fs.readFile(archive.paths.archivedSites + '/' + url, function(err, data) {
+    if (err) {
+      // else direct to loading page
+      this.serveAssets(res, archive.paths.siteAssets, '/loading.html', 'text/html', 302);
+    } else {
+      // append filename to url,
+      res.setHeader('Location', '/' + url);
+      // and redirect
+      res.writeHead(302, {'Content-Type': 'text.html'});
+      res.end();
+    }
+
+  }.bind(this));
+};
