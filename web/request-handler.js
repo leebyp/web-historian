2e7ecf79d97291a0e31a425fc66aeea542b63054
Hack Reactor Students <leebyp@gmail.com>
2014-05-07 20:27:33 -0700

    Cleans up large callbacks into helper modules

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var http = require('./http-helpers.js');
 var url = require('url');
 var fs = require('fs');
 var _ = require('underscore');
@@ -9,43 +10,17 @@ exports.handleRequest = function (req, res) {
 
   var pathName = url.parse(req.url).pathname;
 
-  if (pathName === '/') {
-    if (req.method === 'GET') {
-      fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
-        if (err) {
-          throw err;
-        } else {
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.end(data);
-        }
-      });
-    }
-  } else if (pathName === '/styles.css') {
-    if (req.method === 'GET') {
-      fs.readFile(archive.paths.siteAssets + pathName, function(err, data) {
-        if (err) {
-          throw err;
-        } else {
-          res.writeHead(200, {'Content-Type': 'text/css'});
-          res.end(data);
-        }
-      });
-    }
-  } else {
-    if (req.method === 'GET') {
-      fs.readFile(archive.paths.archivedSites + '/' + pathName, function(err, data) {
-        if (err) {
-          res.writeHead(404);
-          res.end('Not Found');
-        } else {
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.end(data);
-        }
-      });
+  if (req.method === 'GET') {
+    if (pathName === '/') {
+      http.serveAssets(res, archive.paths.siteAssets, '/index.html', 'text/html');
+    } else if (pathName === '/styles.css') {
+      http.serveAssets(res, archive.paths.siteAssets, pathName, 'text/css');
+    } else {
+      http.serveAssets(res, archive.paths.archivedSites, '/' + pathName, 'text/html');
     }
   }
 
-  if (req.method === 'POST') {
+  if (req.method === 'POST'){
     var msg = '';
     req.on('data', function(data){
       msg += data;
@@ -54,47 +29,15 @@ exports.handleRequest = function (req, res) {
       }
     });
     req.on('end', function(){
-
-      fs.readFile(archive.paths.list, function(err, data) {
-        if (err) {
-          throw err;
+      msg = msg.split('=')[1];
+      archive.isUrlInList(msg, function(truthy){
+        if (truthy){
+          http.redirectToArchive(res, msg);
         } else {
-
-          var dataArray = data.toString().split('\n');
-
-          msg = msg.split('=')[1];
-          if (_(dataArray).contains(msg)){
-            // if the file exists in sites folder
-            fs.readFile(archive.paths.archivedSites + '/' + msg, function(err, data) {
-              if (err) {
-                // else direct to loading page
-                fs.readFile(archive.paths.siteAssets + '/loading.html', function(err, data) {
-                  if (err) {
-                    throw err;
-                  } else {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(data);
-                  }
-                });
-              } else {
-                // append filename to url,
-                res.setHeader('Location', '/' + msg);
-                console.log(data);
-                // and redirect
-                res.writeHead(302, {'Content-Type': 'text.html'});
-                res.end();
-              }
-
-            });
-          } else {
-            // direct to loading page and append sitename to sites.txt
-            //
-          }
-
+          archive.addUrlToList(msg);
+          http.serveAssets(res, archive.paths.siteAssets, '/loading.html', 'text/html', 302);
         }
       });
     });
-
-//  res.end(archive.paths.list);
   }
 };
